cb3a78bc8e9d12f7bc115dd42b07700bdd64d48e
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-02-02 14:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-//your JS code here. If required.
-// Create a JavaScript object called student
 const student = {
-  name: "John Doe", // Example name
+  name: "John",
+  getKeys: function() {
+    return Object.keys(this);
+  }
 };
 
-// Function to get the keys of an object
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-// Test the function by passing the student object
-const keys = getKeys(student);
-
-// Verify the result and log it
-console.log(keys); // Should log: ['name']
+// Do not change the code below this line
+window.student = student;
